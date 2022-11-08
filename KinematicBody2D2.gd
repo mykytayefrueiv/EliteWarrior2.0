b82a104315f97416d13aafa4e11d373b27ca23dd
Vsevolod <vsevolod.tarasov@gmail.com>
2022-11-08 16:37:36 +0200

    added moving archer with changing the right to left

---
+++ KinematicBody2D2.gd
@@ -0,0 +1,11 @@
+extends KinematicBody2D
+
+export var speed := 700.0
+func _physics_process(_delta: float) -> void:
+	var input_vector := Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	)
+	var move_direction := input_vector.normalized()
+	move_and_slide(speed *  move_direction)
+	
